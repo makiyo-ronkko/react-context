243821b2191a0d6f8249b0b3afc4b0be0dbd8400
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-11-12 20:25:14 +0200

    Consuming context

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -5,7 +5,7 @@ export const ThemeContext = createContext(); // Consumer and Provider
 export class ThemeProvider extends Component {
   constructor(props) {
     super(props);
-    this.state = { isDarkMode: true };
+    this.state = { isDarkMode: false };
   }
   render() {
     return (
