f29d31c8cd24908314fc541ca406008bc99ac339
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-11-12 20:44:09 +0200

    Create toggleTheme and update a context dynamically

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -6,12 +6,20 @@ export class ThemeProvider extends Component {
   constructor(props) {
     super(props);
     this.state = { isDarkMode: false };
+    this.toggleTheme = this.toggleTheme.bind(this);
   }
+
+  toggleTheme() {
+    this.setState({ isDarkMode: !this.state.isDarkMode });
+  }
+
   render() {
     return (
       // whenever run createContext, get Provider
       // <ThemeContext.Provider value={{isDarkMode: this.state.dark}>{this.props.children}</ThemeContext.Provider>;
-      <ThemeContext.Provider value={{ ...this.state, tastesLikeChicken: true }}>
+      <ThemeContext.Provider
+        value={{ ...this.state, toggleTheme: this.toggleTheme }}
+      >
         {this.props.children}
       </ThemeContext.Provider>
     );
