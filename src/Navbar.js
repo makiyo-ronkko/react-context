850b105a20a4fae534431ffa12a2af4dde095a0c
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-11-15 12:53:40 +0200

    Create a higher order component withLanguageContext

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,42 +9,61 @@ import Switch from '@material-ui/core/Switch';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles/NavBarStyles';
 import { ThemeContext } from './contexts/ThemeContext';
+import { withLanguageContext } from './contexts/LanguageContext';
+
+const content = {
+	english: {
+		search: 'Search',
+		flag: '🇬🇧',
+	},
+	french: {
+		search: 'Chercher',
+		flag: '🇫🇷',
+	},
+	spanish: {
+		search: 'Buscar',
+		flag: '🇪🇸',
+	},
+};
 
 class Navbar extends Component {
-  static contextType = ThemeContext;
+	static contextType = ThemeContext;
 
-  render() {
-    console.log(this.context);
-    const { isDarkMode, toggleTheme } = this.context;
-    const { classes } = this.props;
-    return (
-      <div className={classes.root}>
-        <AppBar position='static' color={isDarkMode ? 'default' : 'primary'}>
-          <Toolbar>
-            <IconButton className={classes.menuButton} color='inherit'>
-              <span>🇫🇷</span>
-            </IconButton>
-            <Typography className={classes.title} variant='h6' color='inherit'>
-              App Title
-            </Typography>
-            <Switch onClick={toggleTheme} />
-            <div className={classes.grow} />
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder='Search...'
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
-              />
-            </div>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
-  }
+	render() {
+		console.log(this.context);
+		const { isDarkMode, toggleTheme } = this.context;
+		const { classes } = this.props;
+		const { language } = this.props.languageContext;
+		// language is coming from this.props.languageContext
+		const { search, flag } = content[language];
+		return (
+			<div className={classes.root}>
+				<AppBar position='static' color={isDarkMode ? 'default' : 'primary'}>
+					<Toolbar>
+						<IconButton className={classes.menuButton} color='inherit'>
+							<span>{flag}</span>
+						</IconButton>
+						<Typography className={classes.title} variant='h6' color='inherit'>
+							App Title
+						</Typography>
+						<Switch onClick={toggleTheme} />
+						<div className={classes.grow} />
+						<div className={classes.search}>
+							<div className={classes.searchIcon}>
+								<SearchIcon />
+							</div>
+							<InputBase
+								placeholder={`${search}...`}
+								classes={{
+									root: classes.inputRoot,
+									input: classes.inputInput,
+								}}
+							/>
+						</div>
+					</Toolbar>
+				</AppBar>
+			</div>
+		);
+	}
 }
-export default withStyles(styles)(Navbar);
+export default withLanguageContext(withStyles(styles)(Navbar));
--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -23,3 +23,13 @@ export class LanguageProvider extends Component {
 		);
 	}
 }
+
+// create high order component, which takes different Component as an argument
+// and additional props, returns the same Component with all original props
+// and injects in languageContext(any names) which is coming from
+// LanguageContext.Consumer. Take the value and passing in to Component.
+export const withLanguageContext = (Component) => (props) => (
+	<LanguageContext.Consumer>
+		{(value) => <Component languageContext={value} {...props} />}
+	</LanguageContext.Consumer>
+);
